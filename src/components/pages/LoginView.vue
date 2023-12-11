a6ce5e61aa6fe919440f0a7e1ac9273ced19644e
Sergey Egorov <ser.12egorov@gmail.com>
2023-12-12 00:02:29 +0300

    LoginView.vue

---
+++ src/components/pages/LoginView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div>
+    <h2>Login</h2>
+    <label for="login">Логин или email</label>
+    <input type="text" name="login" id="login" placeholder="Sereoj" value="">
+    <button/>
+  </div>
+</template>
