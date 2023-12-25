80a7c5021f23276fe30d0f3c40b6583a8bf61129
Sergey Egorov <ser.12egorov@gmail.com>
2023-12-25 18:26:53 +0300

    Route fix

--- src/router/index.js
+++ src/router/index.js
@@ -4,27 +4,7 @@ const routes = [
   {
     path: '/',
     name: 'Index',
-    component: () => import(/* webpackChunkName: "config" */ '@/pages/auth/LoginView.vue')
-  },
-  {
-    path: '/login',
-    name: 'Login',
-    component: () => import(/* webpackChunkName: "config" */ '@/pages/auth/LoginView.vue')
-  },
-  {
-    path: '/register',
-    name: 'Register',
-    component: () => import(/* webpackChunkName: "config" */ '@/pages/auth/RegisterView.vue')
-  },
-  {
-    path: '/forgot-password',
-    name: 'Forgot Password',
-    component: () => import(/* webpackChunkName: "config" */ '@/pages/auth/ForgotView.vue')
-  },
-  {
-    path: '/change-password',
-    name: 'Change Password',
-    component: () => import(/* webpackChunkName: "config" */ '@/pages/auth/ChangePasswordView.vue')
+    component: () => import(/* webpackChunkName: "config" */ '@/layouts/MainLayout')
   },
 ]
 
