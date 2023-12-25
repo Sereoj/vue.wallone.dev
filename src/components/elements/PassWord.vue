65e5fc99334b891152c670507f076598fcdc67e2
Sergey Egorov <ser.12egorov@gmail.com>
2023-12-25 21:12:32 +0300

    Отображение пароля

--- src/components/elements/PassWord.vue
+++ src/components/elements/PassWord.vue
@@ -1,17 +1,27 @@
-<template>
-  <div class="mb-3">
-    <label :for="name" class="form-label">{{ title }}</label>
-    <input type="password" :name="name" :id="name" class="form-control">
-  </div>
-</template>
 <script>
 export default {
   props:[
     'title',
     "name",
-  ]
+  ],
+  data() {
+    return {
+      showPassword: false
+    }
+  }
 }
 </script>
+
+<template>
+  <div class="mb-3">
+    <label :for="name" class="form-label">{{ title }}</label>
+    <input v-bind:type="[showPassword ? 'text' : 'password']" :name="name" :id="name" class="form-control">
+    <button v-on:click="showPassword = !showPassword">
+      Иконка нафиг
+    </button>
+  </div>
+</template>
+
 <style lang="sass" scoped>
 label
   font-size: 24px
