69099a6b4564aa4178dc926794acf8a0d1dc7abe
Sergio Stranger <xabarov-05@mail.ru>
2023-12-25 18:17:04 +0300

    Финальная верстка AuthView

--- src/layouts/AuthLayout.vue
+++ src/layouts/AuthLayout.vue
@@ -1,12 +1,12 @@
 <template>
-  <div class="container-fluid row">
-    <div class="col-5 auth d-none d-md-block">
+  <div class="container-fluid row" style="height: 100vh">
+    <div class="col-5 auth d-none d-lg-block">
 
       <div class="logo">
         <div class="d-flex align-items-center">
           <a href="/" class="logo__name">
-            <img src="@/assets/logo.png" alt="Logo" height="84">
-            <div class="ms-3">
+            <img src="@/assets/logo.png" alt="Logo" height="52" class="me-3">
+            <div>
               <h1>Wallone</h1>
               <a href="/tags/popular" class="logo__brand">#One Day Container</a>
             </div>
@@ -15,11 +15,11 @@
       </div>
     </div>
 
-    <div class="container col-7 m-auto">
+    <div class="container col-sm-12 col-lg-7 m-auto">
 
       <!-- TODO: здесь располагается шапка с ссылками -->
       <div class="col-10 m-auto">
-        <ul class="d-flex mb-5">
+        <ul class="d-flex mb-5" v-if="isAuthPage()">
           <li>
             <router-link to="/login" id="tabLinkLogin" class="h2">Авторизация</router-link>
           </li>
@@ -40,6 +40,16 @@
 export default {
   components: {
     //
+  },
+  data() {
+    return {
+      //
+    }
+  },
+  methods: {
+    isAuthPage() {
+      return this.$route.name === 'Register' || this.$route.name === 'Login'
+    }
   }
 }
 </script>
@@ -57,32 +67,28 @@ export default {
   color: $primary2
 
 .logo
-  margin: 38px 0 0 48px
-  max-width: 15vw
+  margin: 2.38rem 0 0 3.2rem
+  max-width: 20rem
   word-break: break-all
   color: $primary
 
-.logo__name
+.logo__name h1
   @extend %logo-font
-  color: inherit
+  color: $primary
 
 .logo__brand
-  font-size: 1.25rem
+  font-size: 1rem
   color: inherit
-  font-family: "Roboto"
+  font-family: "Roboto", serif
 
 .active
   color: $primary
   padding-bottom: 0.375rem
-  border-bottom: 0.125rem solid $seccond
+  border-bottom: 0.125rem solid $second
 
 .auth
   background: url('@/assets/images/auth.jpg') no-repeat center
   background-size: cover
   object-fit: cover
   height: 100vh
-
-ul li
-  list-style: none
-
 </style>
