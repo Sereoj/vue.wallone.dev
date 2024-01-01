e6a9422481045eeb5642cee7ad5a2b70eba63b2c
Sergey Egorov <ser.12egorov@gmail.com>
2024-01-01 16:38:44 +0300

    Исправлены ссылки в SiteLinks

--- src/components/blocks/SiteLinks.vue
+++ src/components/blocks/SiteLinks.vue
@@ -1,19 +1,20 @@
 <template>
-  <div class="form-text" id="basic-addon4">
-    <router-link to="new">#Новые </router-link>
-    <router-link to="popular">#Популярные </router-link>
+  <div class="form-text" id="site-links">
+    <router-link to="/images/new">#Новые </router-link>
+    <router-link to="/images/popular">#Популярные </router-link>
     <!--      TODO: Важно понимать, что это объект первой видимости, он может пополняться, либо уменьшаться-->
-    <router-link to="popular">#Популярные </router-link>
-    <router-link to="popular">#Популярные </router-link>
-    <router-link to="popular">#Популярные </router-link>
-
-    <router-link to="soon">#Oжидаемые</router-link>
+    <router-link to="/images/popular">#Популярные </router-link>
+    <router-link to="/images/popular">#Популярные </router-link>
+    <router-link to="/images/popular">#Популярные </router-link>
+    <router-link to="/images/soon">#Oжидаемые</router-link>
   </div>
 </template>
 
 <script>
+
 export default {
-  name: "SiteLinks"
+  name: "SiteLinks",
+  components: {}
 }
 </script>
 
--- src/pages/images/NewImagesView.vue
+++ src/pages/images/NewImagesView.vue
@@ -1,6 +1,6 @@
 <template>
 <div>
-
+  <h2>Новые</h2>
 </div>
 </template>
 
