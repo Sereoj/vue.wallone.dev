1021fc129e1f46bba5d97e7a667ccf5a4a1178ab
Sergey Egorov <ser.12egorov@gmail.com>
2024-01-01 21:50:01 +0300

    adbox подключение модуля

--- src/components/ads/AdboxView.vue
+++ src/components/ads/AdboxView.vue
@@ -1,14 +1,30 @@
 <template>
     <div class="d-flex mt-5 d-none d-lg-block">
       <div style="background: #2B2B2B; width: 45vw; height: 4rem;" class="container">
-        <p class="text-center">Реклама, не мобильная</p>
+        <p class="text-center" v-if="isEnable">Отключите пожалуйста Adblock</p>
+        <p class="text-center" v-if="!isEnable">Реклама, не мобильная</p>
       </div>
     </div>
 </template>
 
 <script>
+import { detectAnyAdblocker } from 'vue-adblock-detector'
+
 export default {
   name: "AdboxView",
+  data() {
+    return {
+      'isEnable': false
+    }
+  },
+  computed(){
+    this.adblockCheck()
+  },
+  methods: {
+    adblockCheck(){
+      this.isEnable = detectAnyAdblocker().then((detected) => { return detected })
+    }
+  }
 }
 </script>
 
