5b796368229e48ce64be7c9247ce0cb2f2aad08a
Sergey Egorov <ser.12egorov@gmail.com>
2024-01-05 22:20:07 +0300

    удлаение ненужных пакетов

--- src/pages/images/MainView.vue
+++ src/pages/images/MainView.vue
@@ -1,22 +1,19 @@
 <template>
   <div>
     <h2 class="mb-2 title">Красивые обои на рабочий стол</h2>
-      <p class="description">Wallone - Огромная коллекция красивых изображений на рабочий стол. Cкачивайте, создайте персонализированные темы для приложения  <link-box title="Wallone" name="wallone" path="/landing/wallone"/>(описание).  Подберите интересные фоновые изображения и скачайте их на компьютер в разрешении 2к.</p>
-    <IconBox/>
-    <IconBox/>
-    <IconBox/>
+      <p class="description">Wallone - Огромная коллекция красивых изображений на рабочий стол. Cкачивайте, создайте персонализированные темы для приложения
+        <link-box title="Wallone" name="wallone" path="/landing/wallone"/>(описание).
+        Подберите интересные фоновые изображения и скачайте их на компьютер в разрешении 2к.</p>
   </div>
 </template>
 <style lang="sass">
 @import "@/assets/style/fonts.sass"
 </style>
 <script>
-import IconBox from "@/components/elements/IconBox";
 import LinkBox from "@/components/elements/LinkBox";
 import {useHead} from "@unhead/vue";
 export default {
   components: {
-    IconBox,
     LinkBox
   },
   data() {
