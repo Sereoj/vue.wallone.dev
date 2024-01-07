e41f24d588501cf869a61aac3b13fc362e0b1bc0
Sergey Egorov <ser.12egorov@gmail.com>
2024-01-08 00:50:08 +0300

    SidebarView добавление иконок

--- src/components/blocks/SidebarView.vue
+++ src/components/blocks/SidebarView.vue
@@ -1,71 +1,138 @@
 <template>
   <div class="flex-nowrap">
+<!--    Абстракция-->
+<!--    Автомобили-->
+<!--    Аниме-->
+<!--    Видеоигры-->
+<!--    Визуализация-->
+<!--    Девушки-->
+<!--    Животные-->
+<!--    Космос-->
+<!--    Люди-->
+<!--    Моря-->
+<!--    Природа-->
+<!--    Самолеты-->
+<!--    Фантастика-->
 <!--    <div class="col-auto col-md-3 col-xl-2">-->
     <div>
       <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start navbar-nav" id="menu">
         <li>
-          <a href="#submenu1" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-            <i class="fs-4 bi-speedometer2"></i> <span class="ms-1 d-none d-sm-inline">Категории</span> </a>
-          <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+          <a href="#type" data-bs-toggle="collapse" class="nav-link">
+            <i class="fs-4 i-inline-photograph"></i> <span class="ms-1 d-none d-sm-inline">Тип</span> </a>
+          <ul class="collapse show nav flex-column ms-1" id="type" data-bs-parent="#menu">
+            <li class="w-100">
+              <a href="/themes" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Темы</span></a>
+            </li>
+            <li class="w-100">
+              <a href="/images" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Изображения</span></a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#trends" data-bs-toggle="collapse" class="nav-link">
+            <i class="fs-4 i-inline-fire"></i> <span class="ms-1 d-none d-sm-inline">Тренды</span></a>
+          <ul class="collapse nav flex-column ms-1" id="trends" data-bs-parent="#menu">
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Новинки</span></a>
+            </li>
+            <li>
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Просматриваемые</span></a>
+            </li>
+            <li>
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Самые скачиваемые</span></a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#main" data-bs-toggle="collapse" class="nav-link">
+            <i class="fs-4 i-inline-lightning-bolt"></i> <span class="ms-1 d-none d-sm-inline">Основное</span> </a>
+          <ul class="collapse show nav flex-column ms-1" id="main" data-bs-parent="#menu">
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Мои изображения</span></a>
+            </li>
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Мои коллекции</span></a>
+            </li>
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Мои закладки</span></a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#categories" data-bs-toggle="collapse" class="nav-link">
+            <i class="fs-4 i-outline-view-grid"></i> <span class="ms-1 d-none d-sm-inline">Категории</span> </a>
+          <ul class="collapse show nav flex-column ms-1" id="categories" data-bs-parent="#categories">
             <li class="w-100">
               <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Абстракция</span></a>
             </li>
             <li class="w-100">
-              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Аниме</span></a>
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Автомобили</span></a>
             </li>
             <li class="w-100">
               <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Аниме</span></a>
             </li>
             <li class="w-100">
-              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Аниме</span></a>
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Видеоигры</span></a>
             </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#submenu2" data-bs-toggle="collapse" class="nav-link px-0 align-middle ">
-            <i class="fs-4 bi-bootstrap"></i> <span class="ms-1 d-none d-sm-inline">Теги</span></a>
-          <ul class="collapse nav flex-column ms-1" id="submenu2" data-bs-parent="#menu">
             <li class="w-100">
-              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Популярные</span> 1</a>
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Визуализация</span></a>
             </li>
-            <li>
-              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Популярные</span> 2</a>
-            </li>
-            <li>
-              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Популярные</span> 2</a>
-            </li>
-          </ul>
-        </li>
-        <li>
-          <a href="#submenu3" data-bs-toggle="collapse" class="nav-link px-0 align-middle">
-            <i class="fs-4 bi-grid"></i> <span class="ms-1 d-none d-sm-inline">Products</span> </a>
-          <ul class="collapse nav flex-column ms-1" id="submenu3" data-bs-parent="#menu">
             <li class="w-100">
-              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Product</span> 1</a>
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Девушки</span></a>
             </li>
-            <li>
-              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Product</span> 2</a>
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Животные</span></a>
             </li>
-            <li>
-              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Product</span> 3</a>
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Космос</span></a>
             </li>
-            <li>
-              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Product</span> 4</a>
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Люди</span></a>
+            </li>
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Моря</span></a>
+            </li>
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Природа</span></a>
+            </li>
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Самолеты</span></a>
+            </li>
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Фантастика</span></a>
             </li>
           </ul>
         </li>
         <li>
-          <a href="#" class="nav-link px-0 align-middle">
-            <i class="fs-4 bi-people"></i> <span class="ms-1 d-none d-sm-inline">Customers</span> </a>
+          <a href="#optional" data-bs-toggle="collapse" class="nav-link">
+            <i class="fs-4 i-inline-adjustments"></i> <span class="ms-1 d-none d-sm-inline">Дополнительно</span> </a>
+          <ul class="collapse nav flex-column ms-1" id="optional" data-bs-parent="#menu">
+            <li class="w-100">
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Все теги</span></a>
+            </li>
+            <li>
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Все категории</span></a>
+            </li>
+            <li>
+              <a href="#" class="nav-link"><img src="@/assets/images/auth/1.jpg" alt="icon" class="nav-icon"> <span class="d-none d-sm-inline">Все пользователи</span></a>
+            </li>
+          </ul>
+        </li>
+        <li>
+          <a href="#" class="nav-link">
+            <i class="fs-4 i-inline-newspaper"></i> <span class="ms-1 d-none d-sm-inline">Статьи</span> </a>
         </li>
       </ul>
 
       <ul class="nav navbar-nav gap-3 mt-5">
         <li class="nav-item">
+          <a href="#">О проекте</a>
+        </li>
+        <li class="nav-item">
           <a href="#">Помощь</a>
         </li>
         <li class="nav-item">
-          <a href="#">О сайте</a>
+          <a href="#">Для разработчиков</a>
         </li>
         <li class="nav-item">
           <a href="#">Политика конфиденциальности</a>
@@ -89,4 +156,7 @@
 
 .nav-link:hover
   color: $primary2
+
+.i-outline-view-grid
+  color: #007AFF
 </style>
