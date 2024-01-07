ee493b7fba5a7cc517d4cbe07e2f0bbbb9e9628c
Sergey Egorov <ser.12egorov@gmail.com>
2024-01-07 14:06:08 +0300

    Иконки

---
+++ src/components/elements/Link2Box.vue
@@ -0,0 +1,37 @@
+<template>
+  <template>
+    <router-link :to="path" :id="name" :name="name" class="linkBox">{{ title }}</router-link>
+  </template>
+
+  <script>
+    export default {
+      name: "LinkBox",
+      props:{
+        'title': String,
+        'name': String,
+        'path': String,
+      },
+    }
+  </script>
+
+  <style lang="scss" scoped>
+    @import "@/assets/style/fonts.sass";
+    a{
+      color: $primary2;
+      &:hover
+      {
+        color: $primary;
+      }
+    }
+  </style>
+</template>
+
+<script>
+export default {
+  name: "Link2Box"
+}
+</script>
+
+<style scoped>
+
+</style>
