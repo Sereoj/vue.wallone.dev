c87a410661a38a4411d8fe23746ca935964723ee
Sergey Egorov <ser.12egorov@gmail.com>
2024-01-08 15:09:50 +0300

    Консоль

--- src/main.js
+++ src/main.js
@@ -11,3 +11,6 @@ createApp(App)
     .use(router)
     .use(createHead())
     .mount('#app')
+
+console.log(`%cWallone is started`, 'color: #007AFF')
+console.log(`Welcome Developer! Please read the link %cwallone.ru/devs`, 'color: #007AFF')
